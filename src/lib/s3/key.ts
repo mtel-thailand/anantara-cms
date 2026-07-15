a15f963fe8348b5fdb4ba98ac0fb75d1f3b61db2
Poohdude <suphasan.sae@mtel.co.th>
2026-07-15 14:31:40 +0700

    fix: prefix env

--- src/lib/s3/key.ts
+++ src/lib/s3/key.ts
@@ -1,9 +1,9 @@
 export const buildStorageKey = (filename: string, folder?: "client") => {
   if (folder === "client") {
-    const CLIENT_FOLDER = process.env.AWS_S3_CLIENT_FOLDER;
+    const CLIENT_FOLDER = process.env.S3_CLIENT_FOLDER;
     return `${CLIENT_FOLDER}/${filename}`;
   }
 
-  const CMS_FOLDER = process.env.AWS_S3_CMS_FOLDER;
+  const CMS_FOLDER = process.env.S3_CMS_FOLDER;
   return `${CMS_FOLDER}/${filename}`;
 };
