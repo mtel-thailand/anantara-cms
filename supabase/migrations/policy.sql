1f7c9b8cd1b7a56e0b73f65e5a655debccd72642
Poohdude <suphasan.sae@mtel.co.th>
2026-07-23 10:05:06 +0700

    chore: remove unused

--- supabase/migrations/policy.sql
+++
@@ -1,6 +0,0 @@
-create policy "Enable upload for authenticated user" on "public"."config" as PERMISSIVE for
-UPDATE to authenticated using (
-    true -- Provide a SQL expression for the using statement
-)
-with
-    check (true);
