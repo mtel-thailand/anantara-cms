cdbc09803a3d54e88437c0b8f6475eeac8e3b66e
Poohdude <suphasan.sae@mtel.co.th>
2026-08-05 11:43:21 +0700

    chore: remove unused

--- supabase/migrations/20260730081202_add_owner_reservation_request_note.sql
+++
@@ -1,5 +0,0 @@
-alter table public.owner_reservations
-  add column request_note jsonb not null default '[]'::jsonb;
-
-comment on column public.owner_reservations.request_note is
-  'Request-information history for the owner reservation.';
