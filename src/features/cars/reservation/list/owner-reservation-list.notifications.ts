412c5ab30ce7c300a0282648bd833b312d1f95d8
Poohdude <suphasan.sae@mtel.co.th>
2026-08-10 15:59:12 +0700

    fix: send email

--- src/features/cars/reservation/list/owner-reservation-list.notifications.ts
+++ src/features/cars/reservation/list/owner-reservation-list.notifications.ts
@@ -14,7 +14,7 @@ export async function sendOwnerReservationRequestNotification({
   try {
     if (!context.recipientEmail) throw new Error("Owner email is missing.");
     if (!context.accessToken) throw new Error("Submission access token is missing.");
-    await sendEmail('suphasan.sae@mtel.co.th', {
+    await sendEmail(context.recipientEmail, {
       template: EmailTemplate.OwnerRegistrationRequired,
       params: {
         accessToken: context.accessToken,
